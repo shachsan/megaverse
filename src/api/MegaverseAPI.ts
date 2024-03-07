ff9d3896b92d3b13ce88091f34af6246bc01a533
shachsan <shachsan@hotmail.com>
2024-03-07 12:17:57 -0500

    handle rate limit with retry logic

--- src/api/MegaverseAPI.ts
+++ src/api/MegaverseAPI.ts
@@ -1,25 +1,28 @@
-import axios from 'axios';
 import pLimit from 'p-limit';
+import { axiosWithRetry } from '../utils/requests.js';
 
 const BASE_URL = 'https://challenge.crossmint.io/api';
 const candidateId = 'bf58bc2a-6e06-4339-bb66-81f30e6ba461';
-const limit = pLimit(6); // adjust based on rate limit of megaverse service API'
+const limit = pLimit(1); // adjust based on rate limit of megaverse service API'
 
 class MegaverseAPI {
-  constructor(private candidateId: string) {}
+  constructor(private candidateId: string) { }
 
-  async createPolyanet(row: number, column: number): Promise<void> {
+  createPolyanet(row: number, column: number): void {
     try {
-      await axios.post(`${BASE_URL}/polyanets`, {
-        row,
-        column,
-        candidateId: this.candidateId,
-      });
+      axiosWithRetry({
+        url: `${BASE_URL}/polyanets`,
+        method: 'POST',
+        data: {
+          row,
+          column,
+          candidateId: this.candidateId
+        }
+      })
       console.log(`Polyanet created at (${row}, ${column})`);
     } catch (error: any) {
       console.log(
-        `Failed to create Polyanet at (${row}, ${column}): ${
-          error.response?.data || error.message
+        `Failed to create Polyanet at (${row}, ${column}): ${error.response?.data || error.message
         }`
       );
     }
@@ -29,8 +32,8 @@ class MegaverseAPI {
     const size = 11;
     const promises = [];
 
-    for (let row = 2; row < size; row++) {
-      for (let column = 2; column < size; column++) {
+    for (let row = 2; row < size - 2; row++) {
+      for (let column = 2; column < size - 2; column++) {
         if (row === column || size - 1 - row === column) {
           promises.push(limit(() => this.createPolyanet(row, column)));
         }
---
+++ src/utils/requests.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+
+const DEFAULT_MAX_RETRY_COUNT = 3;
+const INITIAL_DELAY = 1000;
+
+interface AxiosConfig extends AxiosRequestConfig {
+    maxRetryCount?: number;
+    initialDelay?: number;
+}
+export const axiosWithRetry = (config: AxiosConfig) => {
+    let retryAttempt = 0;
+    const attemptRequest = async (): Promise<AxiosResponse> => {
+        try {
+            return await axios(config);
+        } catch (error: any) {
+            const { maxRetryCount = DEFAULT_MAX_RETRY_COUNT, initialDelay = INITIAL_DELAY } = config;
+            ;
+            console.log('axios error', error.toJSON ? error.toJSON() : error)
+            // Determine if we should retry
+            const shouldRetry = retryAttempt <= maxRetryCount &&
+                (error.response?.status === 429 ||
+                    error.code === 'ECONNRESET' ||
+                    error.message.includes('socket hang up'));
+            // console.log('config', config)
+            if (shouldRetry) {
+                retryAttempt++;
+                const backoffDelay = initialDelay * Math.pow(2, retryAttempt) + Math.random() * 100;
+                console.log(`Retry attempt ${retryAttempt}; waiting for ${backoffDelay}ms before retrying...`);
+                await new Promise(resolve => setTimeout(resolve, backoffDelay)); // pause until backoffDelay
+                return attemptRequest();
+            } else {
+                throw error;
+            }
+        }
+    }
+    return attemptRequest();
+}
